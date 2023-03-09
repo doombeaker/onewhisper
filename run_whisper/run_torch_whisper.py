bd1e39fe5928e15adccfe30c0965f46716b267c6
YaoChi <later@usopp.net>
2023-03-09 11:26:24 +0800

    update .gitignore

--- run_whisper/run_torch_whisper.py
+++ run_whisper/run_torch_whisper.py
@@ -8,7 +8,7 @@ import whisper # torch
 import time
 
 if __name__ == "__main__":
-    print("is PyTorch or not:", flow.mock_torch.is_enabled())
+    print("is PyTorch or not:", not flow.mock_torch.is_enabled())
 
     dims_tiny = {'n_mels': 80, 'n_vocab': 51865, 'n_audio_ctx': 1500, 'n_audio_state': 384, 'n_audio_head': 6, 'n_audio_layer': 4, 'n_text_ctx': 448, 'n_text_state': 384, 'n_text_head': 6, 'n_text_layer': 4}
     dims = whisper.ModelDimensions(**dims_tiny)
@@ -21,6 +21,7 @@ if __name__ == "__main__":
 
     start = time.time()
     # import pdb;pdb.set_trace()
-    result = model.transcribe("micro-machines.wav")
+    for i in range(0, 5):
+        result = model.transcribe("micro-machines.wav")
     end = time.time()
     print(f"{end-start} seconds", result["text"])
